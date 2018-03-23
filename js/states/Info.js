e0a0b2ff8c009ecca0f986ad1f5f07987c713b0a
Glissando <justinjoelward@gmail.com>
2018-03-23 15:58:28 -0500

    app check

--- js/states/Info.js
+++ js/states/Info.js
@@ -10,7 +10,7 @@ BasicApp.Info = function(app){
 	//Polygon data
 	this.radius = 50;	//Radius at which the polygon is drawn from
 	this.drawRadius = 10; //radius of the polygon's verticies
-	console.log(app.camera);
+	console.log(app);
 	this.polyCenter = new Phaser.Point(app.width/2, 100); //The polygon's center
 
 	this.back = null;
